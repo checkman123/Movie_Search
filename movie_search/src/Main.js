d0e4401cc3ae79377eb00ec89df1f52c705f7184
checkman123 <checkman3335@gmail.com>
2021-01-27 08:25:50 +0700

    change filename from App to Main

--- movie_search/src/Main.js
+++ movie_search/src/Main.js
@@ -1,7 +1,7 @@
 import logo from './Images/logo.svg';
-import './App.css';
+import './Main.css';
 
-function App() {
+function Main() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +22,4 @@ function App() {
   );
 }
 
-export default App;
+export default Main;
