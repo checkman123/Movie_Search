384264992cadea8a2a4f18264468e403eb55766c
checkman123 <checkman3335@gmail.com>
2021-01-28 15:33:41 +0700

    [frontend] change App file to Main and move logo to Images

--- movie-db/src/Main.js
+++ movie-db/src/Main.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './Images/logo.svg';
+import './Main.css';
 
-function App() {
+function Main() {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default Main;
--- movie-db/src/index.js
+++ movie-db/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Main from './Main';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
