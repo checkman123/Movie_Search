48de1076b53f0fb392ccbb25e7f5273e7714597f
checkman123 <checkman3335@gmail.com>
2021-02-06 12:38:54 +0700

    [client] create a home page

---
+++ movie-db/client/src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import useStyles from './styles';
+
+const Home = () => {
+
+    const classes = useStyles();
+
+  return (
+    <Container>
+        <AppBar className = {classes.appBar} position="static" color="inherit">
+        <Typography className = {classes.heading} variant="h2" align="center">
+            Home Page
+        </Typography>
+        </AppBar>
+    </Container>
+  );
+};
+
+export default Home;
