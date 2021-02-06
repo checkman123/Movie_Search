213f5ea2ece271d232ccd1b2e2482039cc66b8fb
checkman123 <checkman3335@gmail.com>
2021-02-06 12:53:47 +0700

    [client] basic NavBar

--- movie-db/client/src/App.js
+++ movie-db/client/src/App.js
@@ -4,16 +4,17 @@ import { BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 
 import Home from './components/Home/Home'
 import Example from "./components/Example/Example";
+import NavBar from "./components/Navagation/NavBar";
 
 const App = () => {
 
   return (
     <Router>
       <div>
-        {/*<NavBar/>  HERE*/}
+        <NavBar/>
         <div>
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
             </Route>
             <Route path="/example">
