d07a92aaa4f1856a1f21d8e784fcce1717711935
checkman123 <checkman3335@gmail.com>
2021-02-06 10:56:11 +0700

    enable pwa

--- movie-db/client/src/index.js
+++ movie-db/client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { Provider } from 'react-redux'   //<= keep track of global state. Can access state anywhere from app
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
@@ -17,3 +18,6 @@ ReactDom.render(
     </Provider>, 
     document.getElementById("root")
 );
+
+serviceWorkerRegistration.register();
+
