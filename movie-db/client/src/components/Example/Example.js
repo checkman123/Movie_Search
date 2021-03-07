6541ab970201ae24308e411ae01f5530c7e303dc
checkman123 <checkman3335@gmail.com>
2021-03-08 06:09:01 +0700

    [client] simpler Example component

--- movie-db/client/src/components/Example/Example.js
+++ movie-db/client/src/components/Example/Example.js
@@ -1,41 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { Container } from "@material-ui/core";
 
-import {getPosts} from '../../actions/posts'
-import Posts from '../Posts/Posts'
-import Form from '../Form/Form'
-import movies from "../../Images/movies.jpg";
 import useStyles from './styles';
+import { ExampleBoard } from './ExampleBoard';
+import ExampleNav from '../Navigation/ExampleNav';
 
 const Example = () => {
-  const [currentId, setCurrentId] = useState(null);
   const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() =>{
-    dispatch(getPosts());
-  }, [currentId, dispatch])
   return (
     <Container maxwidth="lg">
-      <AppBar className = {classes.appBar} position="static" color="inherit">
-        <Typography className = {classes.heading} variant="h2" align="center">
-          Movies
-        </Typography>
-        <img className = {classes.image} src={movies} alt="movies" height="60" />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId}/>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId}/>
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+      <ExampleNav/>
+      <ExampleBoard/>
     </Container>
   );
 };
