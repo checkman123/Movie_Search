7d8359061f87b36022d1f4f35c30f00fe632025c
Nez <checkman3335@gmail.com>
2021-04-07 05:38:39 +0700

    [client] fix Like button

--- movie-db/client/src/components/Posts/Post/Post.js
+++ movie-db/client/src/components/Posts/Post/Post.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
@@ -13,6 +14,21 @@ import { deletePost, likePost } from '../../../actions/posts'
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
     const dispatch = useDispatch();
+    const user = JSON.parse(localStorage.getItem('profile'));
+
+    //For liking
+    const Likes = () => {
+      if (post.likes.length > 0) {
+        return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+          ? (
+            <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
+          ) : (
+            <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+          );
+      }
+  
+      return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
+    };
 
     return (
         <Card className={classes.card}>
@@ -34,11 +50,14 @@ const Post = ({ post, setCurrentId }) => {
           <Typography variant="body2" color="textSecondary" component="p" gutterBottom>{post.message}</Typography>
         </CardContent>
         <CardActions className={classes.cardActions}>
-          <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> 
-            &nbsp; Like &nbsp;
-            {post.likeCount} 
+          <Button size="small" color="primary" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}>
+            <Likes />
           </Button>
-          <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" /> Delete</Button>
+          {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+          <Button size="small" color="secondary" onClick={() => dispatch(deletePost(post._id))}>
+            <DeleteIcon fontSize="small" /> Delete
+          </Button>
+          )}
         </CardActions>
       </Card>
     );
