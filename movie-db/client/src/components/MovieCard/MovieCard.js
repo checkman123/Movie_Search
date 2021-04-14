afa02b439957ca683dda0ec47e960b0cfabd6841
Nez <checkman3335@gmail.com>
2021-04-14 08:07:38 +0700

    [client] [MovieCard] more styling

--- movie-db/client/src/components/MovieCard/MovieCard.js
+++ movie-db/client/src/components/MovieCard/MovieCard.js
@@ -2,31 +2,35 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import { Link, Route, Switch} from 'react-router-dom'
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
 import useStyles from './styles';
-import MovieInfo from '../MovieInfo/MovieInfo'
+import './styles.scss';
 
 const MovieCard = ({movie}) => {
 
     const classes = useStyles();
 
+    //convert movie title into url pathname
     var str = movie.title;
     str = str.replace(/\s+/g, '-').toLowerCase();
   return (
     <>
-    <Link to={{
+    <div className={classes.card}>    
+      <Link to={{
       pathname: `/movie/${str}`,
       state: { movie },
       }}>
-     <div className={classes.card} key={movie.id}>
-        <img className="card-image"
-            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-            alt={movie.title + ' poster'}
-            />
-        <div className="card-content">
-          <h3 className="card-title">{movie.title}</h3>
+      <div className={classes.cardInner} key={movie.id}>
+        <div className="card-img">
+        <img  src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+              alt={movie.title + ' poster'}/>
         </div>
-      </div>
-    </Link>
+         
+          <h3 className={classes.title}>{movie.title}</h3>
+        </div>
+      </Link>
+    </div>
+
     </>
   );
 };
--- movie-db/client/src/components/MovieCard/styles.js
+++ movie-db/client/src/components/MovieCard/styles.js
@@ -3,13 +3,19 @@ import{ makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
     card: {
       width: '20vw',
-      padding: '2rem 4rem',
       borderRadius: '10px',
       boxShadow: '1px 1px 5px rgba(0,0,0,0.25)',
       marginBottom: '2rem',
       backgroundColor: 'white',
-    }
-    ,
+    },
+    title: {
+      padding: '0.5rem',
+      textAlign: 'center',
+      marginBlock: '0',
+      backgroundColor: 'gray',
+      borderBottomLeftRadius: '10px',
+      borderBottomRightRadius: '10px',
+    },
     /* for phone version */
     [theme.breakpoints.down('sm')] : {
       
