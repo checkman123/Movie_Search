af3f12dc0779601f547de9a6e5fb7335fa52d115
liyanlin1818 <44760974+liyanlin1818@users.noreply.github.com>
2021-05-30 22:07:59 -0700

    last changes

--- movie-db/client/src/components/SearchBars/SearchBars.js
+++ movie-db/client/src/components/SearchBars/SearchBars.js
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import { Link, Route, Switch} from 'react-router-dom'
+import { Link, NavLink, Route, Switch} from 'react-router-dom'
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import useStyles from './styles';
+import { Redirect } from 'react-router-dom';
 import './styles.css'
-
 
 const SearchBars = () => {
     
@@ -12,12 +12,22 @@ const SearchBars = () => {
       //states- input query, movies
       const [query, setQuery] = useState('');
 
+      function handleKeyPress (event) {
+        if(event.key === 'Enter'){
+          console.log('enter press here! ');
+          setQuery(event.target.value);
+          console.log(query);
+          return <Redirect to = {{ pathname: `/search/`, state: { query},}} />
+
+        }
+      }
+
   return (
   
     <div>
         <div class="search">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"/>
-          <input type="text" class="searchTerm" placeholder="What are you looking for?" onChange = {event => setQuery(event.target.value)}/>
+        <input type="text" class="searchTerm" placeholder="What are you looking for?" onChange = {event => setQuery(event.target.value)} onKeyPress={handleKeyPress} />
 
           <Link to={{ pathname: `/search/`, state: { query },}}>
             <button type="submit" class="searchButton" > 
